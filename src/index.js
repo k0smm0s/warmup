eea3c9bbece1a59616f610d4b9958f9287182975
Dmitry Serediuk <k0sm0s@tut.by>
2019-03-13 16:45:16 +0300

    feat: finish warmup function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/**
+ * Change Celsius temperature to Fahrenheit
+ * 
+ * @param {number} temperature Temperature in Celsius
+ * @return {number} tFahrenheit Temperature in Fahrenheit
+ */
+
 module.exports = function warmup(temperature) {
-  // your implementation here
+  var tFahrenheit;
+  tFahrenheit = (temperature*9)/5+32;
+  return tFahrenheit;
 };
